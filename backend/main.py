c58c13ebbcb23234dc841a81f7dfe11f6b8ff078
yamaff <fuga.19671028@icloud.com>
2024-11-22 19:59:01 +0900

    Request

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,18 @@
-print("Hello")
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+class facilitys(BaseModel):
+    name: str
+    latitude: int
+    longitude: int
+
+
+class RequestBodyData(BaseModel):
+    role: str
+    massege: str
+    facilitys: List[facilitys]
+    description: str
+
