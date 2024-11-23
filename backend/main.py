b9f2e57024831c76fc15691be5ecaa1ebe63acaa
yamaff <fuga.19671028@icloud.com>
2024-11-23 11:36:20 +0900

    ts

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,25 @@
+import os
+from dotenv import load_dotenv 
 from fastapi import FastAPI
+from openai import OpenAI
 
+app = FastAPI()
+client = OpenAI()
 
+load_dotenv()
+OpenAI_API_Key = os.environ['OPENAI_API_KEY']
+
+@app.post("/DatePlan")
+async def AI(Request):
+    response = await OpenAI.ChatCompletion.create( 
+        model = "gpt-4o-mini",
+        massages = [{
+            "role": "system",
+            "content": "デートプラン情報を提案するアシスタントです。"
+        }, {
+            "role": "user",
+            "content": f'{Request}'
+        }
+        ]
+    )
+    return response['choices'][0]['massege']['content']
