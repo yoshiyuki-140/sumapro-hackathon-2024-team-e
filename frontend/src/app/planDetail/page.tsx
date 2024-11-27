a1cd5d9871a961c531b28e4d7938bb28c80111ff
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-11-28 00:13:46 +0900

    update : 戻るボタンを追加した

--- frontend/src/app/planDetail/page.tsx
+++ frontend/src/app/planDetail/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 import CustomizedGoogleMap from "@/components/CustomizedGoogleMap";
 import { Facility, SuggestMessage } from "@/types/api";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 // デートプラン詳細ページの関数コンポーネント
 const Detail: React.FC = () => {
+  // リダイレクトを実現するためにuseRouterを使う
+  const router = useRouter();
+
   // セッションストレージからデータ読み出し
   const [suggestDatePlan, setSuggestDatePlan] = useState<SuggestMessage | null>(null);
 
@@ -41,17 +45,30 @@ const Detail: React.FC = () => {
   return (
     <div className="flex h-screen">
       {/* 左側: デートプランの詳細情報 */}
-      <div className="w-1/2 bg-gray-100 p-6 overflow-y-scroll">
+      <div className="w-1/2 h-screen flex flex-col">
+        <div className="h-5/6 bg-gray-100 p-6 overflow-y-scroll">
 
-        {/* 地点情報カード */}
-        {facilities.map((item, index) => (
-          <div
-            key={index}
-            className="bg-white shadow-md rounded-md p-4 mb-4"
+          {/* 地点情報カード */}
+          {facilities.map((item, index) => (
+            <div
+              key={index}
+              className="bg-white shadow-md rounded-md p-4 mb-4"
+            >
+              <h2 className="text-xl font-semibold text-black">{item.name}</h2>
+            </div>
+          ))}
+        </div>
+
+        {/* 戻るボタンとか配置する場所 */}
+        <div className="h-1/6 md p-4 flex justify-center bg-gray-100">
+          {/* ボタンを押すとチャット画面にリダイレクトする */}
+          <button
+            onClick={() => { router.push("/chat") }}
+            className="bg-red-400 p-3 rounded-full border-black border-2 w-36 h-14 text-black font-bold"
           >
-            <h2 className="text-xl font-semibold text-black">{item.name}</h2>
-          </div>
-        ))}
+            戻る
+          </button>
+        </div>
       </div>
 
       {/* 右側: Google Map */}
